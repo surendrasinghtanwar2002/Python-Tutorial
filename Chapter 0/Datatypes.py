04774da86a864ef1a83618513cd2bfef28bfc3f9
Surendra Singh Tanwar <112631512+surendrasinghtanwar2002@users.noreply.github.com>
2024-06-18 23:27:56 +0530

    Latest updates

--- Chapter 0/Datatypes.py
+++ Chapter 0/Datatypes.py
@@ -4,16 +4,13 @@ b = "She is a good instructor as well as good teacher"
 c = 25
 d = 9252417421
 e = 25000.50
-Attendance = 1
+Attendance = True
+PersonalDetails = None
 print("This is name so the datatype of name is", (type(a)))
 print("This is description so the datatype of description is", (type(b)))
 print("This is age so the datatype of age is", (type(c)))
 print("This is mobileno so the datatype of mobileno is", (type(d)))
 print("This is salary of the Shradha Khapra so the datatype of salary will be", (type(e)))
 print("This is attendance of the Shradha Khapra so datatype will be", (type(Attendance)))
-if (Attendance== True or Attendance== False):
-    print("Shradha is present today or either not present today its still checking please wait")
+print("This is Personal Details of the Shradha Khapra so Personal Details is avilable for public", (type(PersonalDetails)))
 
-else:
-    print("No name exist with Shradha Khapra please type a valid name")
-    
