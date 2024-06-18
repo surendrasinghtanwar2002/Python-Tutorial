be413a9c8b2dd0c74857546352a36ede65e90caa
Surendra Singh Tanwar <112631512+surendrasinghtanwar2002@users.noreply.github.com>
2024-06-18 23:53:13 +0530

    Latest Updates

---
+++ Chapter 0/Comments.py
@@ -0,0 +1,17 @@
+# In this section we will learn all the comments which are being used in the python
+
+#If we want to comment single line then we will use simple # like that 
+
+
+print("Hello world")
+
+#If we want to comment multi line then we will use multi line comment using "ctrl + /"
+
+# print("Hello world")
+# print("Hello world")
+# print("Hello world")
+# print("Hello world")
+print("Hello world")
+print("Hello world")
+print("Hello world")
+print("Hello world") 
---
+++ Chapter 0/PractiseSet1.py
@@ -0,0 +1,28 @@
+# In this section we will practise various things of the python such as addition of two number
+
+
+#Adding two operands using addition operator
+a = 25
+b = 35
+c = a+b
+print("The sum of two number is", c)
+
+
+#Subtracting two operands using subtraction operator
+A = 105
+B = 25
+C = A-B
+print("The subtraction of two number is", C)
+
+#Multiplication of two operands using multiple operator
+First = 25
+Second = 35
+Multiplication = First*Second
+print("The multiplication of two number is ", Multiplication, "and datatype is", type(Multiplication))
+
+#Division of two operands using Division operator
+firstno = 35
+Secondno = 2
+division = firstno/Secondno
+print("Division of two number is ", division, "and datatype is" ,type(division))
+
