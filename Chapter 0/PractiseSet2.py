55915dc06c0e74c7ea926989180d3d3f8c463506
Surendra Singh Tanwar <112631512+surendrasinghtanwar2002@users.noreply.github.com>
2024-06-19 12:20:40 +0530

    Added one more section of escape Character

---
+++ Chapter 0/PractiseSet2.py
@@ -0,0 +1,17 @@
+print("Hello world")
+print(5)
+print("Bye")
+print(5+9)
+print(17*13)
+print("Hello guys this is my programming journey where i will learn how to communicate or interact with pc")
+
+
+
+##I am practising python 
+print("Hey i am a good boy")
+
+"""This is multi line comments which can be used or either we can use ctrl+/ forward slash to give the comments in multi line"""
+print("What is the full form of repl? \nSo basically, in Python, we say Read-Evaluate-Print Loop, and this is predefined as a set of code which is provided by standard IDEs.")
+
+#Single line comment
+print("You are accessing a stuff \"Alert\" from the north side that is illegal")
--- Chapter 0/Pritingvariable.py
+++ Chapter 0/Pritingvariable.py
@@ -1,6 +1,7 @@
 ## This is my first python code ever
 print("Hello everyone")
 print("My name is Surendra Singh Tanwar")
+print("Hello my name is Harry potter")
 
 
 ##printing both line in one function
@@ -24,3 +25,10 @@ print("Total of this two number is =", 253+521)
 #Subtracting two number
 print("Subtraction of this two number is = ", 25-15)
 print(650-250)
+
+#Escape sequences
+#Escape sequences are combinations of characters beginning with a backslash (\) used in programming and text processing to represent special characters or perform specific actions that cannot be easily represented within the text. 
+#Examples include \n for newline, \t for tab, and \\ for a literal backslash.
+
+print("Hello my name is Surendra \nI am good right now and I am learning python from the scratch")
+print("I have completed my bachelors from Poornima University\t.This is best college for learning technical stuff and I have gain a lot of knowledge from that college.")
