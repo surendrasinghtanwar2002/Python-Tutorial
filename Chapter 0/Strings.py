b89d8e4b027d1447faac766bdd80402ca089fa2f
Surendra Singh Tanwar <112631512+surendrasinghtanwar2002@users.noreply.github.com>
2024-06-20 09:45:08 +0530

    Final Updates of the code

---
+++ Chapter 0/Strings.py
@@ -0,0 +1,8 @@
+# In this section we will practise on various string concepts to learn string in more better way
+#A string is a sequence of characters. In Python, you can create a string by enclosing text in single quotes ('), double quotes ("), or triple quotes (''' or """).
+
+
+#String is started with single quotes and double quotes
+Name= "Surendra"
+print("Hello", Name)
+
