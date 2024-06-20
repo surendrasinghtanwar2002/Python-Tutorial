c31d32e10b1261b1b1b9a76d910100e5ee35d095
Surendra Singh Tanwar <112631512+surendrasinghtanwar2002@users.noreply.github.com>
2024-06-20 08:53:04 +0530

    Created calculator using ifelse condition

--- Chapter 0/TakingUserinput.py
+++ Chapter 0/TakingUserinput.py
@@ -1,4 +1,10 @@
 #In this section we take input from the user and print the value the user input value in the our console.
-Name = input("Enter your name: ")
-Surname = input("Enter your surname: ")
-print("Your full name is "+Name, Surname)
+Firstname = input("Enter your first name: ")
+Middlename = input("Enter your middle name: ")
+Lastname = input("Enter your last name: ")
+Mobileno = input("Enter your Mobile number: ")
+Password = input("Enter your Password: ")
+print("\nYour Full name is",Firstname,Middlename,Lastname,end="\t\t\t\t\tTrue")
+print("\nYour Mobile number is",Mobileno,end="\t\t\t\t\tTrue")
+print("\nYour Paswword is \"Alert\" you cant see the password due to some policies of the condition", end="\t\t\t\t\t\tTrue")
+
