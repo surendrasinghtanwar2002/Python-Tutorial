afb3d03939609d537e130c2716e77a47005172ec
Surendra Singh Tanwar <112631512+surendrasinghtanwar2002@users.noreply.github.com>
2024-06-20 07:53:04 +0530

    Break

---
+++ Chapter 0/TakingUserinput.py
@@ -0,0 +1,4 @@
+#In this section we take input from the user and print the value the user input value in the our console.
+Name = input("Enter your name: ")
+Surname = input("Enter your surname: ")
+print("Your full name is "+Name, Surname)
