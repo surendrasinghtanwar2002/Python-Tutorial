0ef533c95abf14144a8d9bb1d4475463ed92e1b2
Surendra Singh Tanwar <112631512+surendrasinghtanwar2002@users.noreply.github.com>
2024-06-20 07:11:32 +0530

    Created calculator with practising assignment operator

---
+++ Chapter 0/ArithmeticOperator.py
@@ -0,0 +1,44 @@
+#There are a lot of operators which are being presented in the python so here we will discuss about 
+#the arithmetic operator
+
+#First operator + or Addition operator \" Addition\"
+a = 25
+b = 36
+sum = a+b
+print("sum of", a ,"&", b,"is",sum)
+
+#Second operator is - or Subtraction operator \"Subtraction\"
+c = 1005
+d = 35
+left = c-d
+print("subtraction of",c,"&",d,"is",left)
+
+#Third operator is * or multiplication operator \"Multiplication\"
+f = 35
+g = 45
+mult = f*g
+print("Multiplication of",f, "&",g,"is",mult)
+
+#Fourth operator is / or division operator \"Division\"
+h = 10
+i = 2
+div = h/i
+print("Divison of", h,"with", i,"is equal to", div)
+
+#Fifth operator is ** exponential operator to find out the power of any number
+k = 5
+l = 3
+total = k**l
+print("Power of",k,"uppon",l,"is",total)
+
+#Sixth operator is % or modulus operator which is being to find any division reminder
+m = 21
+n = 3
+modul = m%n
+print("Modulus of", m,"with",n,"is",modul)
+
+#Seventh operator is a floor division operator which is being used to divide the number but not printing value in floating value
+o = 105
+p = 5
+floor = o//p
+print("Floor division of", o, "&",p,"is",floor)
---
+++ Chapter 0/PractiseSet5.py
@@ -0,0 +1,34 @@
+# This is practise set of assignment operator
+
+#Making a calculator using this such operator which are being presented in the python
+
+#adding this both variable number
+A = 105
+B = 5
+sum = A+B
+print("Sum of",A,"&",B,"is",sum)
+
+#subtracting this both variable number
+subtraction = A-B
+print("Subtraction of this both number",A,"&",B,"is",subtraction)
+
+#multiplication of this both variable number
+multiplication = A*B
+print("Multiplication of this both number",A,"&",B,"is",multiplication)
+
+#division of this both variable number is 
+division = A/B
+print("Divison of",A,"&",B,"is",division)
+
+#modulus operator of this both variable number to find the division reminder 
+modulus = A%B
+print("Reminder of",A,B,"is",modulus)
+
+#exponential operator of this both variable to find the power of any number
+expose = A**B
+print("Power of",A,"with",B,"is",expose)
+
+#floor divsion operator to find the reminder of the number without floating point number because whenver we divide the number we get output in the floating value
+#so for solving that problem we use floor division value
+floor = A//B
+print("Floor division of",A,"&",B,"is",floor)
