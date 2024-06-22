2cba1afd5ad7451773e607cf5f198e6deb4996db
Surendra <surendrasinghtanwar667@gmail.com>
2024-06-22 17:19:19 +0530

    Final Updates

---
+++ Chapter 1/ModulusOperator.py
@@ -0,0 +1,5 @@
+a = int(input("Enter your number = "))
+if (a%2 != 0):   #This means that not equal to operator is stick on their decision if we prove that it is equal to 0 then it will say yaa i am false otherwise it will say i am true
+    print("Your number is odd")
+else: 
+    print("Your number is even")
