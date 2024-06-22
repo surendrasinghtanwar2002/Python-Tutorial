ae622dcf801ed70fc5c4d0009e94aaba11643c26
Surendra <surendrasinghtanwar667@gmail.com>
2024-06-22 16:52:01 +0530

    Minor Changes in the code

---
+++ Chapter 1/PractiseStatement7.py
@@ -0,0 +1,7 @@
+# In this project we will basically practise on even and odd number concept using not equal to operator 
+a= int((input("Enter your first no = ")))
+if(a%2 !=0):   #This statement is checking either your a%2 is = 0 if it equal to 0 then it will return false and if it not equal to 0 then it will return true
+    print("You have choosed the odd number")
+else:
+    print("You number is even")
+
