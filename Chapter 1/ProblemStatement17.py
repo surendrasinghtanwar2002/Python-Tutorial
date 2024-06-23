a285caeb277fda7f11535edca827bc6fb33eeffd
Surendra <surendrasinghtanwar667@gmail.com>
2024-06-23 20:23:25 +0530

    Final Updates

--- Chapter 1/ProblemStatement17.py
+++ Chapter 1/ProblemStatement17.py
@@ -3,7 +3,7 @@ Amount = 120  #Avilablemoney
 Withdrawl = int(input("Enter your withdrawl amount = ")) #Trying to withdraw the money
 Bank_Charges = 0.50
 if(Withdrawl%5==0 and Withdrawl+Bank_Charges<Amount and Withdrawl>=0): 
-    Final_amount= Amount-Withdrawl+Bank_Charges # In this statement we deduct the withdrawl amount from the avilableamount
+    Final_amount= Amount-Withdrawl-Bank_Charges # In this statement we deduct the withdrawl amount from the avilableamount
     print("Your pending amount is",Final_amount ) #printed the actual amount after succesful withdrawl
 else:
     if(Withdrawl%5!=0 and Withdrawl<0):
