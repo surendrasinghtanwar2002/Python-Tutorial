0f33c0a0642a90e5a222227cd3cef3242f725d57
Surendra <surendrasinghtanwar667@gmail.com>
2024-07-26 12:41:38 +0530

    New files

--- Project/Simple To Do List Manager/Task_Manager.py
+++ Project/Simple To Do List Manager/Task_Manager.py
@@ -13,14 +13,29 @@ print(welcome_message.center(125,"*"))
 user_menu = ["Add a new Book","View all books","Borrow a book","Return a book","Exit"]
 for index_menu, menu_list in enumerate(user_menu, start= 1):
     print(index_menu, menu_list,":")
+        
 
 ##While Loop
 while True:
     user_input = int(input("Enter your choice from the above list: "))
     if user_input == 1:
         print("You want a new book1")
+    elif user_input == 2:
+        print("You want to view all the book")
+    elif user_input == 3:
+        print("You want to Borrow a book")
+    elif user_input == 4:
+        print("You want to return a book")
+    else:
+        user_exit_choice = (input("You want to exit \"Q\" for quit and \"N\" for return to the menu = "))
+        if user_exit_choice == "q" or "Q":
+            pass
+        break
 
-
-
+##Loop over
+sleep(2)
+os.system('clear')
+end_message = " Thank you Visit Again "
+print(end_message.center(125,"*"))
 
 
