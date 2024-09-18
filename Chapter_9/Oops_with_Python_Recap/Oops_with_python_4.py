6ab4d2803f27f7b7d85a48b53333c3ea9caa7765
Surendra <surendrasinghtanwar667@gmail.com>
2024-09-18 09:54:35 +0530

    New files are being uploaded

--- Chapter_9/Oops_with_Python_Recap/Oops_with_python_4.py
+++ Chapter_9/Oops_with_Python_Recap/Oops_with_python_4.py
@@ -19,12 +19,13 @@ e4 = Email(sender_name="jordan@gmail.com",receiver_name="helly@gmail.com")
 e5 = Email(sender_name="starbucks@gmail.com",receiver_name="surendra@gmail.com")
 
 print(f"Before changes {e1.__dict__}")
+
 ##using the getattr method to get the instance attributes variable value
-print(getattr(e1,"sender_name"))
-print(getattr(e2,"sender_name"))
-print(getattr(e3,"sender_name"))
-print(getattr(e4,"sender_name"))
-print(getattr(e5,"sender_name"))
+print(f"Before Changes {getattr(e1,"sender_name")}")
+print(f"Before Changes {getattr(e2,"sender_name")}")
+print(f"Before Changes {getattr(e3,"sender_name")}")
+print(f"Before Changes {getattr(e4,"sender_name")}")
+print(f"Before Changes {getattr(e5,"sender_name")}")
 
 print(getattr(e1,"receiver_name"))
 print(getattr(e2,"receiver_name"))
@@ -34,6 +35,57 @@ print(getattr(e5,"receiver_name"))
 
 ##using the setattr method to set the instance attributes variable value / Manipulating the instance variable data
 setattr(e1,"sender_name","moras@gmail.com")
+setattr(e2,"sender_name","moras@gmail.com")
+setattr(e3,"sender_name","moras@gmail.com")
+setattr(e4,"sender_name","moras@gmail.com")
+setattr(e5,"sender_name","moras@gmail.com")
+
+setattr(e1,"receiver_name","surendra@gmail.com")
+setattr(e2,"receiver_name","surendra@gmail.com")
+setattr(e3,"receiver_name","surendra@gmail.com")
+setattr(e4,"receiver_name","surendra@gmail.com")
+setattr(e5,"receiver_name","surendra@gmail.com")
+
+print(f"After Changes {getattr(e1,"sender_name")}")
+print(f"After Changes {getattr(e2,"sender_name")}")
+print(f"After Changes {getattr(e3,"sender_name")}")
+print(f"After Changes {getattr(e4,"sender_name")}")
+print(f"After Changes {getattr(e5,"sender_name")}")
+
+print(f"After Changes {getattr(e1,"receiver_name")}")
+print(f"After Changes {getattr(e2,"receiver_name")}")
+print(f"After Changes {getattr(e3,"receiver_name")}")
+print(f"After Changes {getattr(e4,"receiver_name")}")
+print(f"After Changes {getattr(e5,"receiver_name")}")
 
 
-print(f"After changes {e1.__dict__}")
+##using the hasattr method to find the instance variable in the object and if it exist then return True else return False
+print(hasattr(e1,"receiver_name"))          ##Before deleting it will give true
+print(hasattr(e2,"receiver_name"))
+print(hasattr(e3,"receiver_name"))
+print(hasattr(e4,"receiver_name"))
+print(hasattr(e5,"receiver_name"))
+
+delattr(e1,"sender_name")                   ##Deleting the attributes from the object
+delattr(e2,"sender_name")
+delattr(e3,"sender_name")
+delattr(e4,"sender_name")
+delattr(e5,"sender_name")
+
+delattr(e1,"receiver_name")
+delattr(e2,"receiver_name")
+delattr(e3,"receiver_name")
+delattr(e4,"receiver_name")
+delattr(e5,"receiver_name")
+
+print(hasattr(e1,"sender_name"))        ##After deleting the attributes from the object
+print(hasattr(e2,"sender_name"))
+print(hasattr(e3,"sender_name"))
+print(hasattr(e4,"sender_name"))
+print(hasattr(e5,"sender_name"))
+
+
+# setattr(e1,"sender_name","surendra@gmail.com")
+
+print(e1.__dict__)
+
