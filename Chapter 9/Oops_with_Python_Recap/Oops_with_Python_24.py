63bca099e99efecf73326c0a441ab1cdab340d33
Surendra <surendrasinghtanwar667@gmail.com>
2024-09-23 09:40:19 +0530

    New files are being added

--- Chapter 9/Oops_with_Python_Recap/Oops_with_Python_24.py
+++ Chapter 9/Oops_with_Python_Recap/Oops_with_Python_24.py
@@ -1,1 +1,22 @@
                                             ## In this section we will practise MRO Concepts in Python ##
+
+class A:
+    def greet(self):
+        return "Hello from A"
+
+class B(A):
+    def greet(self):
+        return "Hello from B"
+
+class C(A):
+    def greet(self):
+        return "Hello from C"
+
+
+class D(B,C):           ## D Inheriting from B and C
+    pass
+
+##Creating the instance of Class D
+obj = D()
+print(obj.greet())
+print(D.__mro__)
